fa1999479828b591d657efedcae341bfc6873290
Brelon Buckner <brelonbuckner@Brelons-MacBook-Air.local>
2017-12-03 11:57:12 -0500

    Added a while loop for divisions of 3 and 5 between 5 and 50

--- whileLoop.js
+++ whileLoop.js
@@ -27,3 +27,12 @@ while (num <= 333) {
 	num++;
 
 }
+
+console.log("Print all numbers divisible by 5 and 3 between 5 and 50");
+var num = 5;
+while (num <= 50) {
+	if (num % 3 === 0 && num % 5 === 0) {
+		console.log(num);
+	}
+	num++;
+}
