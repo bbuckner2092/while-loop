cef0fe468ebb1ee743a6f81eb457931bcdf3b544
Brelon Buckner <brelonbuckner@Brelons-MacBook-Air.local>
2017-12-03 11:31:07 -0500

    printed all numbers between -10 and 19

--- whileLoop.js
+++ whileLoop.js
@@ -1,1 +1,10 @@
-whileLoop.js
+// Print all numbers between -10 and 19
+// declare a count
+// use a while loop less than 19
+// print the count var and +1
+var count = -10;
+while (count < 19) {
+	console.log(count);
+	count++;
+}
+console.log(count);
